8c47d0ad5a365065ddcbc46a649cf0166a9d05ab
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-15 03:30:34 +0800

    Create AIRHDB_populate_hdb_units.sql

---
+++ sql/AIRHDB_populate_hdb_units.sql
@@ -0,0 +1,4 @@
+INSERT INTO hdb_units VALUES ('test_name', '252, YISHUN RING ROAD', '#07-91', '3-Room', 63, 100, 'Yishun', 'No', 'Hari', '90233232', 
+ 							 1.434725, 103.841021); -- TESTING ONLY
+INSERT INTO hdb_units VALUES ('test_name', '872A, TAMPINES ST 86', '#07-91', '3-Room', 63, 100, 'Tampines', 'No', 'Hari', '90233232', 
+ 							 1.355485, 103.930827); -- TESTING ONLY
