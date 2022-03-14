a7b9e80910f0f0ad2bc9fdc315936acc392a79f9
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-15 02:25:10 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,5 +4,11 @@ from django.db import connection
 # Create your views here.
 def index(request):
     """Shows the main page"""
+    
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM hdb_listings")
+        listings = cursor.fetchall()
+        
+    result_dict = {'records': listings}
 
-    return render(request,'app/index.html',{})
+    return render(request, 'app/index.html', result_dict)
