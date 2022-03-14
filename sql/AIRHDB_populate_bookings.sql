5a384ce619e281c3b2aaf27a82be2d82b4fd93d2
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-15 03:31:31 +0800

    Create AIRHDB_populate_bookings.sql

---
+++ sql/AIRHDB_populate_bookings.sql
@@ -0,0 +1,6 @@
+INSERT INTO bookings VALUES ('252, YISHUN RING ROAD', '#07-91', 'btan@u.nus.edu', 
+							 '2022-04-11', '2022-04-18', 'mastercard', '5100170545952128', 325.68); -- TESTING ONLY
+INSERT INTO bookings VALUES ('872A, TAMPINES ST 86', '#07-91', 'btan@u.nus.edu',
+							 '2022-04-14', '2022-04-19', 'mastercard', '5100170545952128', 325.68); -- TESTING ONLY
+INSERT INTO bookings VALUES ('252, YISHUN RING ROAD', '#07-91', 'btan@u.nus.edu', 
+							 '2022-04-25', '2022-04-27', 'mastercard', '5100170545952128', 325.68); -- TESTING ONLY
