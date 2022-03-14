db2ad86e7f8986f7fd7a456f29cb6238da64204c
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-15 02:16:58 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render, redirect
 from django.db import connection
 
 # Create your views here.
+def index(request):
+    """Shows the main page"""
+
+    return render(request,'app/index.html',{})
