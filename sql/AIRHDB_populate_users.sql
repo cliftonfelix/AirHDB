400ebe71808618be5391adbcbae9e8d25ecb04ec
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-15 03:30:59 +0800

    Create AIRHDB_populate_users.sql

---
+++ sql/AIRHDB_populate_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users VALUES ('Admin', 'admin@airhdb.com', 'AirHDB_admin', '92450330', 'Yes'); -- TESTING ONLY
+INSERT INTO users VALUES ('Bryan Tan', 'btan@u.nus.edu', '12345678', '82342321'); -- TESTING ONLY
