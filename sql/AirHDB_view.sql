dc0d5c7e6006e7c695ccf24bedfcdd2dd7ab4c81
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-15 21:22:15 +0700

    Add ROUND to nearest_mrt

--- sql/AirHDB_view.sql
+++ sql/AirHDB_view.sql
@@ -5,7 +5,8 @@ SELECT hdb1.*,
 	   CASE
 	   		WHEN get_distance(hdb1.hdb_lat, hdb1.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) < 1 
 			THEN CONCAT(mrt1.mrt_name, ' (', (get_distance(hdb1.hdb_lat, hdb1.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) * 1000) :: INT, ' m)')
-			ELSE CONCAT(mrt1.mrt_name, ' (', get_distance(hdb1.hdb_lat, hdb1.hdb_long, mrt1.mrt_lat, mrt1.mrt_long), ' km)')
+			ELSE CONCAT(mrt1.mrt_name, ' (', ROUND(get_distance(hdb1.hdb_lat, hdb1.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) :: NUMERIC, 2)
+						, ' km)')
 	   END nearest_mrt
 FROM hdb_units hdb1, mrt_stations mrt1
 WHERE get_distance(hdb1.hdb_lat, hdb1.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) = 
@@ -13,3 +14,6 @@ WHERE get_distance(hdb1.hdb_lat, hdb1.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) =
 	   FROM hdb_units hdb2, mrt_stations mrt2
 	   WHERE hdb1.hdb_address = hdb2.hdb_address AND hdb1.hdb_unit_number = hdb2.hdb_unit_number
 	   GROUP BY hdb2.hdb_address, hdb2.hdb_unit_number);
+	   
+SELECT *
+FROM hdb_listings;
