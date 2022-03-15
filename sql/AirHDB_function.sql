fcc3b77b8b534a08d60b4e444c4edf54dbc46dca
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-16 00:13:43 +0800

    Add find_nearest_mrt()

--- sql/AirHDB_function.sql
+++ sql/AirHDB_function.sql
@@ -14,3 +14,22 @@ BEGIN
 							  (1 - COS((mrt_long - hdb_long) * PI() / 180)) / 2)));
 END;
 $$;
+
+CREATE OR REPLACE FUNCTION find_nearest_mrt()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS 
+$$
+BEGIN
+	NEW.nearest_mrt := 
+	(SELECT CASE
+		WHEN get_distance(NEW.hdb_lat, NEW.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) < 1 
+			THEN CONCAT(mrt1.mrt_name, ' (', (get_distance(NEW.hdb_lat, NEW.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) * 1000) :: INT, ' m)')
+		ELSE CONCAT(mrt1.mrt_name, ' (', ROUND(get_distance(NEW.hdb_lat, NEW.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) :: NUMERIC, 2), ' km)')
+	END nearest
+	FROM mrt_stations mrt1
+	ORDER BY get_distance(NEW.hdb_lat, NEW.hdb_long, mrt1.mrt_lat, mrt1.mrt_long) ASC
+	LIMIT 1);
+	RETURN NEW;
+END;
+$$;
