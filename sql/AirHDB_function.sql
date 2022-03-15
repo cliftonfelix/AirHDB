87c9cb4dcfc969d76922fbf9794fcaac4cd71cf1
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-15 21:20:43 +0700

    Remove ROUND in get_distance

--- sql/AirHDB_function.sql
+++ sql/AirHDB_function.sql
@@ -9,8 +9,8 @@ language plpgsql
 AS
 $$
 BEGIN
-	RETURN ROUND((12742 * ASIN(SQRT(0.5 - COS((mrt_lat - hdb_lat) * PI() / 180) / 2 + 
-							 		COS(hdb_lat * PI() / 180) * COS(mrt_lat * PI() / 180) *
-									(1 - COS((mrt_long - hdb_long) * PI() / 180)) / 2)))::NUMERIC, 2);
+	RETURN (12742 * ASIN(SQRT(0.5 - COS((mrt_lat - hdb_lat) * PI() / 180) / 2 + 
+							  COS(hdb_lat * PI() / 180) * COS(mrt_lat * PI() / 180) *
+							  (1 - COS((mrt_long - hdb_long) * PI() / 180)) / 2)));
 END;
 $$;
