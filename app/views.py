b121a135b9399b588719cc6169ae073873cdfc51
JasonCP14 <88238739+JasonCP14@users.noreply.github.com>
2022-03-17 01:28:39 +0700

    Add login_page function

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.db import connection
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -12,3 +15,20 @@ def index(request):
     result_dict = {'records': units}
 
     return render(request, 'app/index.html', result_dict)
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, 'User does not exist!')
+        
+        user = authenticate(request, username = username)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+
+    context = {}
+    return render(request, 'app/login.html', context)
