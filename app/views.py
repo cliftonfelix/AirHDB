430e2c3c58b5f1598bb55b48796376aa8c584de5
JasonCP14 <88238739+JasonCP14@users.noreply.github.com>
2022-03-17 22:53:15 +0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -20,51 +20,41 @@ def login_page(request):
             messages.error(request, 'Invalid Username!')
      
         user = authenticate(request, username = email, password = password)
-        if user is not None: #check if user is admin or not from users table
+        if user is not None:
             login(request, user)
             return redirect('listings')
         else:
             messages.error(request, 'Wrong password entered!')
-    context = {}
-    return render(request, 'app/login.html', context)
+            
+    return render(request, 'app/login.html')
 
 def logout_page(request):
     logout(request)
     return redirect('login')
 
 def register_page(request):
-    form = UserCreationForm()
-    
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit = False)
-            user.username = user.username.lower()
-            user.save()
-            login(request, user)
-            return redirect('listings')
-        else:
-            messages.error(request, 'An error occured during registration')
-        """
+        # Ensure password matches confirmation
         name = request.POST.get('name')
-        username = request.POST.get('username') #change to email - check if email inside django users or users database (either 1)
-        password = request.POST.get('password') #
-        # mobile number -> request post get
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
-        
-        # before putting into Django -> need to TRY putting into users database and catch if there is any error with integrity constraint 
-        # (email alr exists or mobile number violates check)
-        # then add the user to django
-        try:
-            user = User.objects.get(username = username)
-        except:
-            messages.error(request, 'User does not exist!')
-            """
-        
-        """
-        
-        """
-    return render(request, 'app/register.html', {'form': form})
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM user WHERE email = %s", [email])
+            row = cursor.fetchone()
+            if password != confirmation:
+                messages.error(request, 'Passwords do not match!')
+
+                ## No user with same email
+            if row == None:
+                cursor.execute("INSERT INTO users VALUES (%s, %s, %s, %s, "no"), [name, email, password, number])
+                user = User.objects.create_user(email, password = password)
+                user.save()
+                login(request, user)
+                return redirect('listings')
+            else:
+                messages.error(request, 'The email has been used by another user!')
+    return render(request, 'app/register.html')
     
 @login_required(login_url = 'login')
 def listings(request):
