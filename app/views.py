4a85d9c754d6bcfdb65fcbb3ec8b448f61e7e175
JasonCP14 <88238739+JasonCP14@users.noreply.github.com>
2022-03-17 19:32:28 +0700

    Add todo

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 
 # Create your views here.
 def login_page(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated: #check if user is admin or not from users table
         return redirect('listings')
     
     if request.method == 'POST':
@@ -20,11 +20,11 @@ def login_page(request):
             messages.error(request, 'User does not exist!')
      
         user = authenticate(request, username = email, password = password)
-        if user is not None:
+        if user is not None: #check if user is admin or not from users table
             login(request, user)
             return redirect('listings')
 
-    context = {}
+    context = {} #message wrong password
     return render(request, 'app/login.html', context)
 
 def logout_page(request):
@@ -46,14 +46,23 @@ def register_page(request):
             messages.error(request, 'An error occured during registration')
         """
         name = request.POST.get('name')
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get('username') #change to email - check if email inside django users or users database (either 1)
+        password = request.POST.get('password') #
+        # mobile number -> request post get
         confirm_password = request.POST.get('confirm_password')
+        
+        # before putting into Django -> need to TRY putting into users database and catch if there is any error with integrity constraint 
+        # (email alr exists or mobile number violates check)
+        # then add the user to django
         try:
             user = User.objects.get(username = username)
         except:
             messages.error(request, 'User does not exist!')
             """
+        
+        """
+        
+        """
     return render(request, 'app/register.html', {'form': form})
     
 @login_required(login_url = 'login')
