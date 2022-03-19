25718e59c18230d2b60a9cc06d6b94fac97e8841
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-19 19:50:22 +0800

    Add Jason's branch

--- app/views.py
+++ app/views.py
@@ -1,10 +1,84 @@
-from django.shortcuts import render, redirect
 from django.db import connection
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login_page(request):
+    if request.user.is_authenticated:
+        email = request.user.username
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM users WHERE email_address = %s", [email])
+            row = cursor.fetchone()
+            if row[3] == 'Yes':
+                return redirect('admin')
+            elif row[3] == 'No':
+                return redirect('listings')
+        
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+        try: 
+            user = User.objects.get(username = email)
+        except:
+            messages.error(request, 'Invalid email address!')
+            return render(request, 'app/login.html')  
+        user = authenticate(request, username = email, password = password)
+        if user is not None:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM users WHERE email_address = %s", [email])
+                row = cursor.fetchone()
+                login(request, user)
+                if row[3] == 'Yes':
+                    return redirect('admin')
+                elif row[3] == 'No':
+                    return redirect('listings')
+        else:
+            messages.error(request, 'Wrong password entered!')
+            return render(request, 'app/login.html')
+    return render(request, 'app/login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
+def register_page(request):
+    if request.method == 'POST':
+        # Ensure password matches confirmation
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+        if password != confirm_password:
+            messages.error(request, 'Passwords do not match!')
+            return render(request, 'app/register.html')
+        
+        with connection.cursor() as cursor:
+            try: 
+                cursor.execute("INSERT INTO users VALUES (%s, %s, %s)", [name, email, number])
+            except Exception as e:
+                string = str(e)
+                message = ""
+                if 'duplicate key value violates unique constraint "users_pkey"' in string:  
+                    message = 'The email has already been used by another user!'
+                elif 'new row for relation "users" violates check constraint "users_email_address_check"' in string:
+                    message = 'Please enter a valid email address!'
+                elif 'new row for relation "users" violates check constraint "users_mobile_number_check"' in string:
+                    message = 'Please enter a valid Singapore number!'
+                messages.error(request, message)
+                return render(request, 'app/register.html')
+            
+            user = User.objects.create_user(email, password = password)
+            user.save()
+            messages.success(request, 'Account has been successfully registered!')
+            return redirect('login')
+    return render(request, 'app/register.html')
+    
+@login_required(login_url = 'login')
 def listings(request):
     """Shows the main page"""
     
@@ -16,28 +90,6 @@ def listings(request):
 
     return render(request, 'app/index.html', result_dict)
 
-def login_page(request):
-#     with connection.cursor() as cursor:
-#         cursor.execute("SELECT * FROM users")
-#         users = cursor.fetchall()
-       
-#     for user in users:
-#         user_temp = User.objects.create_user(user[2], password = user[1])
-#         user_temp.save()
-        
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-#         try:
-#             user = User.objects.get(username = username)
-#         except:
-#             messages.error(request, 'User does not exist!')
-        
-    
-        user = authenticate(request, username = username, password = password)
-        if user is not None:
-            login(request, user)
-            return redirect('listings')
-
-    context = {}
-    return render(request, 'app/login.html', context)
+@login_required(login_url = 'login')
+def admin(request):
+    return render(request, 'app/admin.html')
