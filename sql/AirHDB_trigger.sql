663023f9e111b164f8a324d1df4a4f144b3e578a
JasonCP14 <88238739+JasonCP14@users.noreply.github.com>
2022-03-19 15:14:34 +0700

    Delete sql directory

--- sql/AirHDB_trigger.sql
+++
@@ -1,5 +0,0 @@
-DROP TRIGGER IF EXISTS insert_nearest_mrt ON hdb_units;
-
-CREATE TRIGGER insert_nearest_mrt
-BEFORE INSERT OR UPDATE ON hdb_units
-FOR EACH ROW EXECUTE PROCEDURE find_nearest_mrt();
