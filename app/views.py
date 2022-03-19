a300282252fe1732f8cdfe77c8ac168adcecbe53
JasonCP14 <88238739+JasonCP14@users.noreply.github.com>
2022-03-19 16:59:21 +0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, redirect
 from django.db import connection
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-import re
 
 # Create your views here.
 def login_page(request):
