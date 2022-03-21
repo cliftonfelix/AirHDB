e138b21f739718cdca21602e63f3822b21353143
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-21 20:35:26 +0800

    Add new_mrt_station trigger

--- sql/AirHDB_trigger.sql
+++ sql/AirHDB_trigger.sql
@@ -1,6 +1,7 @@
 DROP TRIGGER IF EXISTS insert_nearest_mrt ON hdb_units;
 DROP TRIGGER IF EXISTS new_total_price ON bookings;
 DROP TRIGGER IF EXISTS booking_dates_availability ON bookings;
+DROP TRIGGER IF EXISTS new_mrt_station ON mrt_stations;
 
 CREATE TRIGGER insert_nearest_mrt
 BEFORE INSERT OR UPDATE ON hdb_units
@@ -13,3 +14,7 @@ FOR EACH ROW EXECUTE PROCEDURE change_total_price();
 CREATE TRIGGER booking_dates_availability
 BEFORE UPDATE ON bookings
 FOR EACH ROW EXECUTE PROCEDURE check_dates_availability();
+
+CREATE TRIGGER new_mrt_station
+AFTER INSERT ON mrt_stations
+FOR EACH ROW EXECUTE PROCEDURE update_all_nearest_mrts();
