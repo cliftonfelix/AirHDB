8b197a198beefb8ecae683c1403a9d3792bf91ff
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-22 19:25:57 +0800

    Add Type Info

--- sql/AirHDB_view.sql
+++ sql/AirHDB_view.sql
@@ -2,6 +2,7 @@ DROP VIEW IF EXISTS hdb_listings;
 
 CREATE VIEW hdb_listings AS
 SELECT *
-FROM hdb_units
+FROM hdb_units hu1
+NATURAL JOIN hdb_types_info hti1
 WHERE can_book = 'Yes'
 ORDER BY hdb_id;
