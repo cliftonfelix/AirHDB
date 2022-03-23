b52f5e4b96db2136b8ca05f04015af41c47de002
JasonCP14 <88238739+JasonCP14@users.noreply.github.com>
2022-03-23 15:10:36 +0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -397,6 +397,7 @@ def change_profile(request):
             return render(request, 'app/change_profile.html', context)
 	
         cursor.execute("UPDATE users SET name = %s, mobile_number = %s WHERE email_address = %s", [name, number, email])
+	return redirect('profile')
     """
         except Exception as e:
             string = str(e)
@@ -406,5 +407,5 @@ def change_profile(request):
             return render(request, 'app/change_profile.html', context)
         messages.success(request, 'Profile has been successfully updated!')
     """
-    return redirect('profile')
-    #return render(request, 'app/change_profile.html', context)
+    
+    return render(request, 'app/change_profile.html', context)
