b627a6d2a1615a4f3700022978fdd45917ee21bb
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-03-23 16:11:18 +0800

    Change javascript

--- app/views.py
+++ app/views.py
@@ -108,7 +108,7 @@ def listings(request):
     result_dict['num_guests'] = ''
     result_dict['min_price_per_day'] = ''
     result_dict['max_price_per_day'] = ''
-    result_dict['regions_default'] = '' #TODO: Default value
+    result_dict['regions_default'] = [] #TODO: Default value
     result_dict['towns_default'] = '' #TODO: Default value
     result_dict['hdb_types_default'] = '' #TODO: Default value
     result_dict['min_size'] = ''
@@ -175,6 +175,7 @@ def listings(request):
         if regions:
             temp = ""
             for region in regions:
+                result_dict['regions_default'] += [region]
                 if temp:
                     temp += " UNION "
                 temp += """{0} 
