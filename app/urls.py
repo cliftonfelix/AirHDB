7b33f38c6f76a7da275c75400a49124989e3bab9
hadragon26 <87232777+hadragon26@users.noreply.github.com>
2022-03-24 23:14:00 +0800

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,13 +6,13 @@ from . import views
 urlpatterns = [
     
 
-    path('adminbookings/',views.adminb,name='adminbookings'),
-    path('adminunits/',views.adminu,name='adminunits'),
-    path('adminunits/view/<str:id>/',views.viewunits),
-    path('adminbookings/view/<str:id>/',views.viewbookings),
-    path('adminunits/edit/<str:id>/',views.editunits),
-    path('adminbookings/edit/<str:id>/',views.editbookings),
-    path('adminunits/add/',views.addunits,name='adminaddunits')
+    path('bookings/',views.adminb,name='adminbookings'),
+    path('units/',views.adminu,name='adminunits'),
+    path('units/view/<str:id>/',views.viewunits),
+    path('bookings/view/<str:id>/',views.viewbookings),
+    path('units/edit/<str:id>/',views.editunits),
+    path('bookings/edit/<str:id>/',views.editbookings),
+    path('units/add/',views.addunits,name='adminaddunits')
     
 
 ]
