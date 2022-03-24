44b5a7b81546cea417c6090a9b2d69762d0461cd
JasonCP14 <88238739+JasonCP14@users.noreply.github.com>
2022-03-24 20:08:30 +0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -435,7 +435,7 @@ def bookings(request):
     email = request.user.username
     with connection.cursor() as cursor:
         cursor.execute("SELECT b.booking_id, b.hdb_id, h.hdb_address, h.hdb_unit_number, b.start_date, b.end_date, b.credit_card_type, b.credit_card_number, b.total_price\
-		       FROM bookings b, hdb_units h WHERE b.hdb_id = h.hdb_id AND booked_by = %s", [email])
+		       FROM bookings b, hdb_units h WHERE b.hdb_id = h.hdb_id AND booked_by = %s ORDER BY b.booking_id", [email])
         bookings = cursor.fetchall()
     context = {}
     context['bookings'] = bookings
