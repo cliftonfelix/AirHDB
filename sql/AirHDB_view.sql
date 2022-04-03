7fe75ce755c8e45e8ba091fb0d1a83d03b74c1c2
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-04-03 19:34:09 +0800

    Update AirHDB_view.sql

--- sql/AirHDB_view.sql
+++ sql/AirHDB_view.sql
@@ -1,4 +1,5 @@
 DROP VIEW IF EXISTS hdb_listings;
+DROP VIEW IF EXISTS refunds_details;
 
 CREATE VIEW hdb_listings AS
 SELECT *
@@ -7,3 +8,9 @@ NATURAL JOIN hdb_types_info hti1
 NATURAL JOIN towns t1
 WHERE can_book = 'Yes'
 ORDER BY hdb_id;
+
+CREATE VIEW refunds_details AS
+SELECT *
+FROM refunds
+NATURAL JOIN bookings
+ORDER BY booking_id;
