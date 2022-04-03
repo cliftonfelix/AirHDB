574500553b82f3385180230b43cc14d7986c5643
cliftonfelix <88199596+cliftonfelix@users.noreply.github.com>
2022-04-03 20:06:41 +0800

    Remove refunds_details

--- sql/AirHDB_view.sql
+++ sql/AirHDB_view.sql
@@ -1,5 +1,4 @@
 DROP VIEW IF EXISTS hdb_listings;
-DROP VIEW IF EXISTS refunds_details;
 
 CREATE VIEW hdb_listings AS
 SELECT *
@@ -8,9 +7,3 @@ NATURAL JOIN hdb_types_info hti1
 NATURAL JOIN towns t1
 WHERE can_book = 'Yes'
 ORDER BY hdb_id;
-
-CREATE VIEW refunds_details AS
-SELECT *
-FROM refunds
-NATURAL JOIN bookings
-ORDER BY booking_id;
